c2a8d383e2a74f7973d14d930cff1a7289dc67fc
Scott Lusk <scott.t.lusk@gmail.com>
2024-02-06 16:46:59 -0600

    wip

--- src/steam_update.sh
+++ src/steam_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/.steam/sdk64/
+mkdir -p /home/steam/.steam/sdk64
 su -c "${STEAMCMDDIR}/steamcmd.sh +login anonymous +app_update 1007 +quit" steam
-cp ~/Steam/steamapps/common/Steamworks\ SDK\ Redist/linux64/steamclient.so ~/.steam/sdk64/
+cp '/home/steam/Steam/steamapps/common/steamworks/linux64/steamclient.so' /home/steam/.steam/sdk64/
 
 su -c "${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} validate +quit" steam
